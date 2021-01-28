361e25b3717b2712ec5a8069cb5ecebf7d93ca69
=Chethan-170 <chethanpoojary170@gmail.com>
2021-01-28 20:54:45 +0530

    some functionalities added

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,9 +1,11 @@
 export const NavBar = () => {
+
     return (  
         <nav className="navbar navbar-light bg-primary text-white">
-          <h2 className="navbar-text ml-2 text-white">
-            Google Books Store 
-          </h2>
+          <h1 className="navbar-text ml-2 text-white">
+            Google Books Store  
+          </h1>
+          <button className="btn btn-sm float-right" onClick={()=>{window.history.back()}}>Back</button>
         </nav>
     );
 }
